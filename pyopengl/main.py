fb0b7976510c366bc371e5445e3b1678ac788ce6
Daniel Mateos <daniel@mateos.cc>
2020-12-29 20:04:15 +1100

    refactor main file

--- pyopengl/main.py
+++ pyopengl/main.py
@@ -3,55 +3,61 @@ from renderer import window, primitives, shader
 import glfw
 import pyrr
 
-main_window = window.Window(1024, 768)
-main_window.run()
 
-with open("renderer/shaders/basic_frag.gsl") as f:
-    frag_data = f.read()
-with open("renderer/shaders/basic_vert.gsl") as f:
-    vert_data = f.read()
+class Screen:
+    def __init__(self):
+        self.window = window.Window(1024, 768)
+        self.window.run()
 
-basic_frag = shader.Shader("fragment", frag_data)
-basic_vert = shader.Shader("vertex", vert_data)
+        self.window.add_key_callback(self.key_handler)
+        self.window.reset_mouse_position()
+        self.window.add_mouse_callback(self.mouse_handler)
 
-program = shader.Program()
-program.add_shader(basic_frag)
-program.add_shader(basic_vert)
-program.validate()
-program.compile()
-program.use()
+        with open("renderer/shaders/basic_frag.gsl") as f:
+            frag_data = f.read()
+        with open("renderer/shaders/basic_vert.gsl") as f:
+            vert_data = f.read()
 
-perspective = pyrr.Matrix44.perspective_projection(75.0, 1280 / 1024, 0.1, 1000.0)
-program.set_uniform("perspective", perspective)
+        basic_frag = shader.Shader("fragment", frag_data)
+        basic_vert = shader.Shader("vertex", vert_data)
 
-sprites = [
-    primitives.Rectangle(program, 0, 0, 0.5, 0.5, [1.0, 0.0, 0.0]),
-]
+        self.program = shader.Program()
+
+        self.program.add_shader(basic_frag)
+        self.program.add_shader(basic_vert)
+        self.program.validate()
+        self.program.compile()
+        self.program.use()
+
+        perspective = pyrr.Matrix44.perspective_projection(75.0, 1280 / 1024, 0.1, 1000.0)
+        self.program.set_uniform("perspective", perspective)
+
+        self.sprites = [
+            primitives.Rectangle(self.program, 0, 0, 0.5, 0.5, [1.0, 0.0, 0.0]),
+        ]
 
 
-def key_handler(key, code, action, mods):
-    if key == glfw.KEY_W:
+    def update(self):
+        self.window.clear()
+
+        for sprite in self.sprites:
+            sprite.x += 0.001
+            sprite.draw()
+
+        self.window.swap()
+
+    def key_handler(self, key, code, action, mods):
+        if key == glfw.KEY_DOWN:
+            pass
+        elif key == glfw.KEY_UP:
+            pass
+
+
+    def mouse_handler(self, xpos, ypos):
         pass
-    elif key == glfw.KEY_S:
-        pass
-    elif key == glfw.KEY_A:
-        pass
-    elif key == glfw.KEY_D:
-        pass
 
+if __name__ == "__main__":
+    screen = Screen()
 
-def mouse_handler(xpos, ypos):
-    main_window.reset_mouse_position()
-
-
-main_window.add_key_callback(key_handler)
-main_window.add_mouse_callback(mouse_handler)
-
-while main_window.ok():
-    main_window.clear()
-
-    for sprite in sprites:
-        sprite.x += 0.001
-        sprite.draw()
-
-    main_window.swap()
+    while True:
+        screen.update()
