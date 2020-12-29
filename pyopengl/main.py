de35254fe304340389b002b99b0e1effd5b3d8b1
Daniel Mateos <daniel@mateos.cc>
2020-12-29 20:37:58 +1100

    start of pong equiv pygame in 2d gl engine imp

--- pyopengl/main.py
+++ pyopengl/main.py
@@ -4,9 +4,42 @@ import glfw
 import pyrr
 
 
+SCREEN_SIZE = (1024, 1000)
+USER_COLOR = (0, 0, 1)
+COMPUTER_COLOR = (0, 0, 1)
+
+
+class Paddle:
+    def __init__(self, program, computer: bool = False):
+        if computer:
+            self.rect = primitives.Rectangle(program, 0, 0, 0.1, 0.4, COMPUTER_COLOR)
+            self.rect.x += 0.50
+        else:
+            self.rect = primitives.Rectangle(program, 0, 0, 0.1, 0.4, USER_COLOR)
+            self.rect.x -= 0.50
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.rect.draw()
+
+
+class Ball:
+    def __init__(self, program):
+        self.rect = primitives.Rectangle(program, 0, 0, 0.05, 0.05, (1, 0, 0))
+        self.rect.x += 0.25
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.rect.draw()
+
+
 class Screen:
     def __init__(self):
-        self.window = window.Window(1024, 768)
+        self.window = window.Window(SCREEN_SIZE[0], SCREEN_SIZE[1])
         self.window.run()
 
         self.window.add_key_callback(self.key_handler)
@@ -29,19 +62,26 @@ class Screen:
         self.program.compile()
         self.program.use()
 
-        perspective = pyrr.Matrix44.perspective_projection(75.0, 1280 / 1024, 0.1, 1000.0)
+        perspective = pyrr.Matrix44.perspective_projection(
+            75.0, 1280 / 1024, 0.1, 1000.0
+        )
         self.program.set_uniform("perspective", perspective)
 
-        self.sprites = [
-            primitives.Rectangle(self.program, 0, 0, 0.5, 0.5, [1.0, 0.0, 0.0]),
-        ]
+        self.user_paddle = Paddle(self.program)
+        self.computer_paddle = Paddle(self.program, True)
+        self.ball = Ball(self.program)
 
+        self.sprites = [
+            self.user_paddle,
+            self.computer_paddle,
+            self.ball,
+        ]
 
     def update(self):
         self.window.clear()
 
         for sprite in self.sprites:
-            sprite.x += 0.001
+            sprite.update()
             sprite.draw()
 
         self.window.swap()
@@ -51,13 +91,13 @@ class Screen:
 
     def key_handler(self, key, code, action, mods):
         if key == glfw.KEY_DOWN:
-            pass
+            self.user_paddle.rect.y -= 0.1
         elif key == glfw.KEY_UP:
-            pass
-
+            self.user_paddle.rect.y += 0.1
 
     def mouse_handler(self, xpos, ypos):
         pass
+
 
 if __name__ == "__main__":
     screen = Screen()
