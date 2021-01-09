88ea7464da025a9a1b9a21d8dc8d6b5d69cb7d90
Daniel Mateos <daniel@mateos.cc>
2021-01-10 04:41:25 +1100

    remove old debug code

--- pyopengl/renderer/entities.py
+++ pyopengl/renderer/entities.py
@@ -5,13 +5,12 @@ TEXTURE_COORDINATES = (1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0)
 
 
 class ObjectFile(object):
-    def __init__(self, path, debug=False):
+    def __init__(self, path):
         self.path = path
         self.verts = []
         self.normals = []
         self.vert_index = []
         self.norm_index = []
-        self.debug = debug
         self.read()
 
     def read(self):
@@ -30,12 +29,6 @@ class ObjectFile(object):
                 for vert in line.split(" ")[1:]:
                     self.vert_index.append(int(vert.split("//")[0]) - 1)
                     self.norm_index.append(int(vert.split("//")[1]) - 1)
-
-        if self.debug:
-            print("ObjReader")
-            print(self.verts)
-            print(self.vert_index)
-            print(self.norm_index)
 
     def get_geometry(self):
         return [
