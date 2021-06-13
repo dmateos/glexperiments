b5857d584329c7f5f645179fc368fc01906a9343
Daniel Mateos <daniel@mateos.cc>
2021-06-13 20:55:52 +1000

    start on obj file parser

--- cgl/model.c
+++ cgl/model.c
@@ -7,6 +7,7 @@
 
 static void parse_obj_file(const char *path) {
     char *model_data;
+    char *line;
 
     model_data = read_file(path);
     if (!model_data) {
@@ -14,7 +15,20 @@ static void parse_obj_file(const char *path) {
         exit(1);
     }
 
-    printf("%s\n", model_data);
+    line = strtok(model_data, "\n");
+    while (line != NULL) {
+        if (strncmp(line, "vn", strlen("vn")) == 0) {
+            printf("vn line: %s\n", line);
+        } else if (strncmp(line, "f", strlen("f")) == 0) {
+            printf("f line: %s\n", line);
+        } else if (strncmp(line, "v", strlen("v")) == 0) {
+            printf("v line: %s\n", line);
+        } else {
+            printf("unknown line: %s\n", line);
+        }
+        line = strtok(NULL, "\n");
+    }
+
     free_file_data(model_data);
 }
 
