ad6b8fa5bac68383e3d7ee70f9830fe950d9372f
Daniel Mateos <daniel@mateos.cc>
2021-06-16 02:02:17 +1000

    finish obj parser

--- cgl/model.c
+++ cgl/model.c
@@ -5,10 +5,12 @@
 
 #include "util.h"
 
-static void parse_obj_file(const char *path) {
-    char *model_data, *line, *tok;
-    char *line_state, *token_state = NULL;
+static int parse_obj_file(ObjFile *obj, const char *path) {
+    char *model_data, *line, *line_state;
+    float x, y, z;
+    unsigned int n1, n2, n3, v1, v2, v3;
 
+    memset(obj, 0, sizeof(ObjFile));
     model_data = read_file(path);
     if (!model_data) {
         printf("could not load model %s\n", path);
@@ -18,43 +20,38 @@ static void parse_obj_file(const char *path) {
     line = strtok_r(model_data, "\n", &line_state);
     while (line != NULL) {
         if (strncmp(line, "vn", strlen("vn")) == 0) {
-            printf("vn line: %s\n", line);
-            tok = strtok_r(line, " ", &token_state);
-            tok = strtok_r(NULL, " ", &token_state);
-            while (tok != NULL) {
-                printf("vn token: %s\n", tok);
-                if (tok[strlen(tok)] == '\n') {
-                    tok = NULL;
-                } else {
-                    tok = strtok_r(NULL, " ", &token_state);
-                }
-            }
+            sscanf(line, "vn %f %f %f\n", &x, &y, &z);
+            printf("vn %f %f %f\n", x, y, z);
+            obj->normals[obj->ncount++] = x;
+            obj->normals[obj->ncount++] = y;
+            obj->normals[obj->ncount++] = z;
         } else if (strncmp(line, "v", strlen("v")) == 0) {
-            printf("v line: %s\n", line);
-            tok = strtok_r(line, " ", &token_state);
-            tok = strtok_r(NULL, " ", &token_state);
-            while (tok != NULL) {
-                printf("v token: %s\n", tok);
-                if (tok[strlen(tok)] == '\n') {
-                    tok = NULL;
-                } else {
-                    tok = strtok_r(NULL, " ", &token_state);
-                }
-            }
+            sscanf(line, "v %f %f %f\n", &x, &y, &z);
+            printf("v %f %f %f\n", x, y, z);
+            obj->verticies[obj->vcount++] = x;
+            obj->verticies[obj->vcount++] = y;
+            obj->verticies[obj->vcount++] = z;
         } else if (strncmp(line, "f", strlen("f")) == 0) {
-            printf("f line: %s\n", line);
-        } else {
-            printf("unknown line: %s\n", line);
+            sscanf(line, "f %d//%d %d//%d %d//%d\n", &v1, &n1, &v2, &n2, &v3,
+                   &n3);
+            printf("f: %d//%d %d//%d %d//%d\n", v1, n1, v2, n2, v3, n3);
+            obj->verticie_index[obj->vicount++] = v1;
+            obj->verticie_index[obj->vicount++] = v2;
+            obj->verticie_index[obj->vicount++] = v3;
+            obj->normal_index[obj->nicount++] = n1;
+            obj->normal_index[obj->nicount++] = n2;
+            obj->normal_index[obj->nicount++] = n3;
         }
         line = strtok_r(NULL, "\n", &line_state);
-        token_state = NULL;
     }
 
     free_file_data(model_data);
+    return 0;
 }
 
 int init_model(Model *model, const char *path) {
+    ObjFile obj;
     memset(model, 0, sizeof(Model));
-    parse_obj_file(path);
+    parse_obj_file(&obj, path);
     return 0;
 }
--- cgl/model.h
+++ cgl/model.h
@@ -4,6 +4,8 @@
 #include "shader.h"
 #include "vertex.h"
 
+#define OBJ_VERTEX_LIMIT 1024
+
 typedef struct _Model {
     float x, y, z;
     ShaderProgram *program;
@@ -12,6 +14,13 @@ typedef struct _Model {
     float *verts, *normals;
     int *vert_index, *norm_index;
 } Model;
+
+typedef struct _Obj {
+    float verticies[OBJ_VERTEX_LIMIT], normals[OBJ_VERTEX_LIMIT];
+    unsigned int verticie_index[OBJ_VERTEX_LIMIT];
+    unsigned int normal_index[OBJ_VERTEX_LIMIT];
+    unsigned int ncount, vcount, vicount, nicount;
+} ObjFile;
 
 int init_model(Model *, const char *);
 
